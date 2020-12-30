95cf725d83b9220effae8b852cec7dbdfe604050
KanghoonYi <rkdgnsdls@gmail.com>
2020-12-30 16:18:06 +0900

    set Jekyll

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Datadog Agent Nvml",
+        "excerpt":"datadog-agent-nvml  for using nvidia graphic metric on datadog this project is working with docker.  What is NVML?  Monitoring NVIDIA GPUs status using Datadog. see https://github.com/ngi644/datadog_nvml see https://developer.nvidia.com/nvidia-management-library-nvml   go to repository  https://github.com/KanghoonYi/datadog-agent-nvml  ","categories": [],
+        "tags": [],
+        "url": "https://kanghoonyi.github.io/datadog-agent-nvml/",
+        "teaser": null
+      }]
